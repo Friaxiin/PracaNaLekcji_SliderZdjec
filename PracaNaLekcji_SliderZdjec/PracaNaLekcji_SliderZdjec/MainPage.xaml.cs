f8a18cd1fc4dad4650121cf7550412f506497813
patryk.labuz <patryk.labuz@S50-11>
2024-04-03 09:42:19 +0200

    Dodaj pliki projektów.

---
+++ PracaNaLekcji_SliderZdjec/PracaNaLekcji_SliderZdjec/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PracaNaLekcji_SliderZdjec
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DelImg(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GoToGallery(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
