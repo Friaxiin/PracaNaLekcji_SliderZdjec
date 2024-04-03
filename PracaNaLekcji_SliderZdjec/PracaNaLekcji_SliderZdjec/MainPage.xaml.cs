7e879068e6b1e3406cf487e5d3923ffbcf4e51ed
patryk.labuz <patryk.labuz@S50-11>
2024-04-03 09:51:04 +0200

    Dodanie kodu na ładownie obrazów

--- PracaNaLekcji_SliderZdjec/PracaNaLekcji_SliderZdjec/MainPage.xaml.cs
+++ PracaNaLekcji_SliderZdjec/PracaNaLekcji_SliderZdjec/MainPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace PracaNaLekcji_SliderZdjec
@@ -14,15 +16,35 @@ namespace PracaNaLekcji_SliderZdjec
         {
             InitializeComponent();
         }
+        private void LoadImg()
+        {
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Images");
+            List<Image> images = new List<Image>();
+
+            if(Directory.Exists(path))
+            {
+                string[] imgFiles = Directory.GetFiles(path);
+
+                foreach(var imgFile in imgFiles)
+                {
+                    images.Add(new Image { Source = FileImageSource.FromFile(imgFile), Name = imgFile });
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ścieżka do folderu nie istnieje");
+            }
+            imgCarousel.ItemsSource = images;
+        }
 
         private void DelImg(object sender, EventArgs e)
         {
 
         }
 
-        private void GoToGallery(object sender, EventArgs e)
+        private async void GoToGallery(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
